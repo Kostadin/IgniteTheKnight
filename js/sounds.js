a2123ff368bbcca481b64fe187bb6926f4d00c81
Neven Iliev <n.iliev89@gmail.com>
2015-01-24 11:24:14 +0200

    Fix JS error, move player sprite

--- js/sounds.js
+++ js/sounds.js
@@ -1,21 +1,24 @@
 ﻿//Audio objects
-var backgroundTrack = new Audio("audio/.mp3");
+var backgroundTrack = null;//new Audio("audio/.mp3");
 //Looping
+/*
 backgroundTrack.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
+*/
+var wilhelm = null;//new Audio("audio/WilhelmScream.mp3");
 
-var wilhelm = new Audio("audio/WilhelmScream.mp3");
+var gameOver = null;//new Audio("audio/Game_Over.mp3");
 
-var gameOver = new Audio("audio/Game_Over.mp3");
+var playerDeath = null;//new Audio("audio/Death.mp3");
 
-var playerDeath = new Audio("audio/Death.mp3");
-
-var mainTheme = new Audio("audio/Main_Title_Theme.mp3");
+var mainTheme = null;//new Audio("audio/Main_Title_Theme.mp3");
+/*
 mainTheme.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 
 mainTheme.play();
+*/
