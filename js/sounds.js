992ec0144403e021097dabe459d7db1a35281cfb
Neven Iliev <n.iliev89@gmail.com>
2015-01-24 22:37:52 +0200

    Blank sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,11 +1,12 @@
 ﻿//Audio objects
 var backgroundTrack = new Audio("audio/blank.mp3");
 //Looping
+/*
 backgroundTrack.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-
+*/
 var wilhelm = new Audio("audio/blank.mp3");
 
 var gameOver = new Audio("audio/blank.mp3");
@@ -13,10 +14,10 @@ var gameOver = new Audio("audio/blank.mp3");
 var playerDeath = new Audio("audio/blank.mp3");
 
 var mainTheme = new Audio("audio/blank.mp3");
-
+/*
 mainTheme.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-
+*/
 mainTheme.play();
