275cb5785e33e0fc79b1117be0a03b33b2f7b998
Neven Iliev <n.iliev89@gmail.com>
2015-01-24 11:31:48 +0200

    Placeholder mp3

--- js/sounds.js
+++ js/sounds.js
@@ -1,24 +1,22 @@
 ﻿//Audio objects
-var backgroundTrack = null;//new Audio("audio/.mp3");
+var backgroundTrack = new Audio("audio/blank.mp3");
 //Looping
-/*
 backgroundTrack.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-*/
-var wilhelm = null;//new Audio("audio/WilhelmScream.mp3");
 
-var gameOver = null;//new Audio("audio/Game_Over.mp3");
+var wilhelm = new Audio("audio/blank.mp3");
 
-var playerDeath = null;//new Audio("audio/Death.mp3");
+var gameOver = new Audio("audio/blank.mp3");
 
-var mainTheme = null;//new Audio("audio/Main_Title_Theme.mp3");
-/*
+var playerDeath = new Audio("audio/blank.mp3");
+
+var mainTheme = new Audio("audio/blank.mp3");
+
 mainTheme.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 
 mainTheme.play();
-*/
