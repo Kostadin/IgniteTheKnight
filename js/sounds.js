d221f7fac3a3aec1628b52a20b9bdc82eb852911
Neven Iliev <n.iliev89@gmail.com>
2015-01-25 12:00:13 +0200

    Music tracks

--- js/sounds.js
+++ js/sounds.js
@@ -1,23 +1,23 @@
 ﻿//Audio objects
-var backgroundTrack = new Audio("audio/blank.mp3");
+var backgroundTrack = new Audio("audio/BGM_-_the_lava_is_nigh.mp3");
 //Looping
-/*
+
 backgroundTrack.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-*/
+
 var wilhelm = new Audio("audio/blank.mp3");
 
 var gameOver = new Audio("audio/blank.mp3");
 
 var playerDeath = new Audio("audio/blank.mp3");
 
-var mainTheme = new Audio("audio/blank.mp3");
-/*
+var mainTheme = new Audio("audio/Title_-_welcome_to_hell.mp3");
+
 mainTheme.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-*/
+
 mainTheme.play();
