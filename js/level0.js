54192bf8bf7e29269e753c049237a418c1bb3317
Neven Iliev <n.iliev89@gmail.com>
2015-01-25 09:52:08 +0200

    Add new tile textures

--- js/level0.js
+++ js/level0.js
@@ -1,28 +1,44 @@
 ﻿levels[0]={
 	def:[
-		{
+		{//0
 			type: lava,
 			texture: "img/sprites/tiles.png"
 		},
-		{
+		{//1
 			type: platform,
 			texture: "img/sprites/tiles.png"
 		},
-		{
+		{//2
 			type: portal,
 			texture: "img/tiles/Portal.png"
 		},
-		{
+		{//3
 			type: wall,
-			texture: "img/tiles/Wall.png"
-		}
+			texture: "img/tiles/Wall1.png"
+		},
+		{//4
+			type: wall,
+			texture: "img/tiles/rightWall.png"
+		},
+		{//5
+			type: wall,
+			texture: "img/tiles/topWall.png"
+		},
+		{//6
+			type: wall,
+			texture: "img/tiles/leftWall.png"
+		},
+		{//7
+			type: wall,
+			texture: "img/tiles/filler3.png"
+		},
 	],
 	map:[
 		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0],
-		[1,1,1,1,1,1,1,3,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,1,3,0,1,1,1,0,3,0,0,0],
-		[0,0,0,0,0,0,1,3,0,1,0,1,0,0,0,0,0],
+		[3,3,3,3,3,3,3,6,4,0,0,0,0,0,0,0,0],
+		[1,1,1,1,1,1,1,6,4,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,1,6,4,1,1,1,0,3,0,0,0],
+		[0,0,0,0,0,0,1,3,3,1,0,1,0,0,0,0,0],
 		[0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,0],
 		[0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0],
 		[0,0,0,0,0,0,1,1,1,0,0,1,1,1,0,1,0],
